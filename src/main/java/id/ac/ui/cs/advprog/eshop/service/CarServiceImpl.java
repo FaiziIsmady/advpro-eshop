9760d112d78ab7708691849a17e373de48e6093b
FaiziIsmady <faiziismady5@gmail.com>
2025-02-28 09:55:54 +0700

    refactor: (SRP) Removed redundant conversion of Iterator<Car>

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -16,17 +16,13 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public Car create(Car car) {
-        // TODO Auto-generated method stub
         carRepository.create(car);
         return car;
     }
 
     @Override
     public List<Car> findAll() {
-        Iterator<Car> carIterator = carRepository.findAll();
-        List<Car> allCar = new ArrayList<>();
-        carIterator.forEachRemaining(allCar::add);
-        return allCar;
+        return carRepository.findAll();
     }
 
     @Override
@@ -37,13 +33,11 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public void update(String carId, Car car) {
-        // TODO Auto-generated method stub
         carRepository.update(carId, car);
     }
 
     @Override
     public void deleteCarById(String carId) {
-        // TODO Auto-generated method stub
         carRepository.delete(carId);
     }
 }
