e162049e3109578eab881813ad36ae04e69ae9bf
FaiziIsmady <faiziismady5@gmail.com>
2025-03-07 17:17:51 +0700

    [GREEN] Make PaymentVoucher to implement its tests

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucher.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucher.java
@@ -6,21 +6,40 @@ public class PaymentVoucher {
     private String voucherCode;
     
     public PaymentVoucher(String voucherCode) {
+        this.voucherCode = voucherCode;
     }
     
     public String getVoucherCode() {
-
+        return voucherCode;
     }
     
     public void setVoucherCode(String voucherCode) {
-
+        this.voucherCode = voucherCode;
     }
     
     public PaymentStatus validateVoucher() {
-
+        if (!isValidVoucherFormat()) {
+            return PaymentStatus.REJECTED;
+        }
+        return PaymentStatus.SUCCESS;
     }
     
     private boolean isValidVoucherFormat() {
+        if (voucherCode == null || voucherCode.length() != 16) {
+            return false;
+        }
 
+        if (!voucherCode.startsWith("ESHOP")) {
+            return false;
+        }
+
+        int numericCount = 0;
+        for (char c : voucherCode.toCharArray()) {
+            if (Character.isDigit(c)) {
+                numericCount++;
+            }
+        }
+
+        return numericCount >= 8;
     }
 }
