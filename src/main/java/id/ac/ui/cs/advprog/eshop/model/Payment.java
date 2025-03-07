af74113c4245c045f19c3935003c696c9f42adc7
FaiziIsmady <faiziismady5@gmail.com>
2025-03-07 12:18:16 +0700

    [GREEN] Make Payment model adheres to PaymentTest

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -12,4 +12,39 @@ public class Payment {
     private String status;
     private Map<String, String> paymentData;
     private String orderId;
+
+    public Payment(String id, String method, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.status = "REJECTED"; // Default status
+        setPaymentData(paymentData);
+    }
+
+    public Payment(String id, String method, Map<String, String> paymentData, String status) {
+        this(id, method, paymentData);
+        setStatus(status);
+    }
+
+    public void setStatus(String status) {
+        if (isValidStatus(status)) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void setPaymentData(Map<String, String> paymentData) {
+        if (paymentData == null || paymentData.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        this.paymentData = paymentData;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    private boolean isValidStatus(String status) {
+        return status.equals("SUCCESS") || status.equals("REJECTED");
+    }
 }
