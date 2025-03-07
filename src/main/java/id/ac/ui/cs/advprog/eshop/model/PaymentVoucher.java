ef4ec21abe6349da98c9bf84c5d09bd692e89898
FaiziIsmady <faiziismady5@gmail.com>
2025-03-07 17:15:38 +0700

    [RED] Added PaymentVoucher skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucher.java
@@ -0,0 +1,26 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+
+public class PaymentVoucher {
+    private String voucherCode;
+    
+    public PaymentVoucher(String voucherCode) {
+    }
+    
+    public String getVoucherCode() {
+
+    }
+    
+    public void setVoucherCode(String voucherCode) {
+
+    }
+    
+    public PaymentStatus validateVoucher() {
+
+    }
+    
+    private boolean isValidVoucherFormat() {
+
+    }
+}
